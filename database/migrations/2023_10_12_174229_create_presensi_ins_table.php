0e70308305b8280fcc501f528e930515a27a6db8
Wahab Muhammad <wahabmuhammad95@gmail.com>
2025-09-13 07:51:45 +0700

    update tabel presensi

--- database/migrations/2023_10_12_174229_create_presensi_ins_table.php
+++ database/migrations/2023_10_12_174229_create_presensi_ins_table.php
@@ -13,12 +13,20 @@ return new class extends Migration
     {
         Schema::create('presensi', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->integer('nip');
+            $table->string('shift', 255);
+            $table->string('name', 255)->nullable();
+            $table->string('nip', 11)->nullable();
             $table->date('tgl_presensi');
             $table->time('jam_in');
-            $table->string('foto_in');
-            $table->text('location_in');
+            $table->string('foto_in', 255);
+            $table->text('location_in')->nullable();
+            $table->date('tgl_presensi_out')->nullable();
+            $table->time('jam_out')->nullable();
+            $table->string('foto_out', 255)->nullable();
+            $table->text('location_out')->nullable();
+            $table->string('total_jamkerja', 255)->nullable();
+            $table->string('jam_terlambat', 255)->nullable();
+            $table->string('user_fk', 255)->nullable();
             $table->timestamps();
         });
     }
