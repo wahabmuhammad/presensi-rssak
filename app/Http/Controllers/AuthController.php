71fef331c71818aaca15f20b3cbea3adb58ec6a6
Wahab Muhammad <wahabmuhammad95@gmail.com>
2025-10-06 22:44:50 +0700

    fix bug authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,55 +3,57 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Redirect;
 use PhpParser\Node\Stmt\Echo_;
 
 
 class AuthController extends Controller
 {
-    public function proseslogin(Request $request){
-        // if(Auth::guard('pegawai')->attempt(['nip' => $request->nip, 'password' => $request->password])){
-        //     return redirect('/dashboard');
-        // }else{
-        //     return redirect('/');
-        // }
+    public function proseslogin(Request $request)
+    {
 
         $request->validate(
             [
-            'nip' => 'required',
-            'password' => 'required'
-            ],[
+                'nip' => 'required',
+                'password' => 'required'
+            ],
+            [
                 'nip.required' => 'NIP wajib diisi',
                 'password.required' => 'password wajib diisi',
-            ]);
+            ]
+        );
 
         $infologin = [
-            'nip'=>$request->nip,
-            'password'=>$request->password,
+            'nip' => $request->nip,
+            'password' => $request->password,
         ];
 
-        if(Auth::attempt($infologin)){
-            if(Auth::user()->role == 'admin'){
+        $user = User::whereRaw('nip ILIKE ?', [$request->nip])->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            Auth::login($user);
+            if ($user->role == 'admin') {
                 return redirect('/admin');
-            }else{
+            } else {
                 return redirect('/dashboard');
             }
-        }else{
+        } else {
             return redirect('/')->withErrors('NIP atau Password tidak sesuai')->withInput();
         }
-        
     }
     public function logout(Request $request)
-        {
-            Auth::logout();
-    
-            request()->session()->invalidate();
-    
-            request()->session()->regenerateToken();
-    
-            return redirect('/');
-        }
+    {
+        Auth::logout();
+
+        request()->session()->invalidate();
+
+        request()->session()->regenerateToken();
+
+        return redirect('/');
+    }
 }
