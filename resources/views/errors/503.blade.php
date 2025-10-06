2cf226189f7878d938c522d2543da30da61e364a
Wahab Muhammad <wahabmuhammad95@gmail.com>
2025-10-06 08:33:45 +0700

    fix

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', __('Service Unavailable'))
 @section('code', '503')
-@section('message', __('Server sedang Update'))
+@section('message', __('Server dinonaktifkan. Silakan gunakan link presensi baru.'))
 @section('extra')
     <div style="margin-top: 20px; text-align:center;">
         <a href="https://presensi.rssarkiesaisyiyahkudus.co.id/"
